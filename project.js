61b91884f68d5017864442f384bcc29e547d0ffd
monalisazyx <madhurimazyx@gmail.com>
2023-09-12 17:09:16 +0530

    Add Psuedocode

---
+++ project.js
@@ -0,0 +1,7 @@
+//return random from rock paper and scissors --getcomputerchoice
+
+//function takes two paprameters and returns who wins 
+   //computer selection and player selection
+   //conditionals to see who wins
+//play five rounds -- game
+//keep score of and declare who wins
