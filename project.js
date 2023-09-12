d27a720da0b2e20527c35171be823b0b6ad9e195
monalisazyx <madhurimazyx@gmail.com>
2023-09-12 17:20:42 +0530

    Output random choice from computer

--- project.js
+++ project.js
@@ -1,6 +1,14 @@
 //return random from rock paper and scissors --getcomputerchoice
+function getComputerChoice(){
+    let array = ["rock", "paper" , "scissors"]
 
-//function takes two paprameters and returns who wins 
+    let random = Math.floor(Math.random()*array.length);
+    let str = array[random];
+    return str;
+
+}
+
+//function takes two parameters and returns who wins 
    //computer selection and player selection
    //conditionals to see who wins
 //play five rounds -- game
