50a8abc1c1dac95f43f49b018b1349a6a51c461f
monalisazyx <madhurimazyx@gmail.com>
2023-09-15 07:29:48 +0530

    Add feature to calculate final score

--- project.js
+++ project.js
@@ -13,95 +13,108 @@ function getComputerChoice(){
 function playRound(){
     let comp = getComputerChoice();
     let player = prompt("Enter choice: ").toLowerCase();
-    let compCount = 0;
-    let playerCount = 0;
-   
+    let countPlayer = 0;
+    let countComp = 0;
     if (comp == "rock" && player=="paper"){
-        playerCount += 1;
-        console.log ("You win. Paper beats rock");
+        console.log("You win. Paper beats rock");
+        countPlayer += 1;
     }
     else if (comp == "paper" && player=="rock"){
-        compCount += 1;
-        console.log ("You lose. Paper beats rock");
+        console.log("You lose. Paper beats rock");
+        countComp+= 1;
 
     }
     else if (comp == "paper" && player=="scissors"){
-        playerCount += 1;
-
-        console.log ("You win. Paper beats rock");
-
-
+        console.log("You win. scissors beats paper");
+        countPlayer += 1;
 
     }
     else if (comp == "scissors" && player=="paper"){
-        compCount += 1;
-
-        console.log ("You lose. Paper beats rock");
-
-
+        console.log("You lose. scissors beats paper");
+        countComp+= 1;
 
     }
     else if (comp == "scissors" && player=="rock"){
-        compCount += 1;
-
-        console.log ("You lose. Paper beats rock");
-
-
+        console.log("You lose. scissors beats rock");
+        countComp+= 1;
 
     }
     else if (comp == "rock" && player=="scissors"){
-        playerCount += 1;
-
-        console.log ("You win. Paper beats rock");
-
-
+        console.log("You win. scissors beats rock");
+        countPlayer += 1;
 
     }
     else {
-        console.log ("tie");
+        console.log("tie");
     }
-    return [compCount , playerCount];
+    return [countComp , countPlayer];
+   
+    
+
+
+
+   
     
 
 
 }
-function storePlayerCount( playerCount){
-    return playerCount;
 
-}
-function storeComCount(compCount){
-    return compCount;
-}
 let i = 0;
+let compFinal = 0;
+let playerFinal = 0;
+  
+//}
 
-function game(){
-    for(let i = 0; i < 3 ; i++){
-        
-        let [x,y] = playRound();
-        
-        
-        console.log(score(x , y));
-
-
-
-    }  
-}
 
 function score(comp , player ){
+    let winnerPlayer = true;
+    let winnerComp = true;
     if(comp > player){
-        console.log ('You lose');
+        winnerComp = true;
     }
     else if(player>comp){
-        console.log ('You win');
+        winnerPlayer = true;
     }
     else {
-        console.log ("It's a tie");
+        winnerPlayer = false;
+        winnerComp = false;
     }
+    return [winnerComp, winnerPlayer];
+
 }
 
+function game(){
+    //for(let i = 0; i < 3 ; i++){
+        
+        let [x,y] = playRound();
+        console.log(`player score ${y} , computer score ${x}`);
+        let z = score(x , y);
+        let compWin , playerWin;
+        [winnerComp , winnerPlayer] = [compWin , playerWin] ;
+        if(compWin === false && playerWin === true){
+            playerFinal += 1;
+        
+        }
+        else if (playerWin !== true && compWin === true ){
+            compFinal += 1;
+        }
+        else {
+            return "equal scores";
+        }
+        return [compFinal , playerFinal];
+
+
+
+    }
 
 game();
+game();
 
+let x;
+let y;
+[x,y ]= [compFinal , playerFinal];
+console.log(`player final score : ${y}`);
+console.log(`computer final score : ${x}`);
 
 
 
